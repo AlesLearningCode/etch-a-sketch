cced2d6dec4b93ecd8f13bc01e18320db49ec66f
AlesLearningCode <146716296+AlesLearningCode@users.noreply.github.com>
2023-10-11 14:46:52 +0200

    Add a mouse hover for painting
    Added a function so whenever mouse hovers over squares it leaves a
    trail

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,13 +1,31 @@
 const content = document.querySelector(`#container`);
+let gridRow;
 
 function createGrid(size){
-content.style.width = 12 * size + "px";
+content.style.width = 5 * size + "px";
 for(let i = 0; i < (size * size); i++){
-    const gridRow = document.createElement(`div`)
+    gridRow = document.createElement(`div`)
     content.appendChild(gridRow);
     gridRow.classList.add(`row`)
 }
 }
- createGrid(100)
+
+createGrid(50)
 
 
+
+    let highlight = document.querySelectorAll(".row")
+    highlight.forEach((i) => {
+        i.addEventListener('mouseover', () =>{
+            i.setAttribute("style", "background-color:white;")
+        });
+        i.addEventListener('mouseout', () =>{
+            i.setAttribute("style", "background-color:green;")
+        });
+    });
+
+
+  
+
+
+    
