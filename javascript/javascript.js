767c8c6d2959d60e50f98e3e083b9ae23c9d8e4c
AlesLearningCode <146716296+AlesLearningCode@users.noreply.github.com>
2023-10-12 22:11:04 +0200

    Add style
    added some styles, still need improvements

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,46 +1,48 @@
 const content = document.querySelector(`#container`);
 const btn = document.querySelector(`button`)
 let size;
-let clicked;
+let trailStart;
 
     function createGrid(size){
         for(let i = 0; i < (size * size); i++){
             gridRow = document.createElement(`div`);
             content.appendChild(gridRow);
             gridRow.classList.add(`row`)  
-            gridRow.style.height = `${960 / size}px`;  
-            gridRow.style.width = `${960 / size}px`;   
+            gridRow.style.height = `${959 / size}px`;  
+            gridRow.style.width = `${959 / size}px`;   
     }
 }
     
+
     btn.addEventListener(`click`, () => {
-        while (container.firstChild) {
-            container.removeChild(container.firstChild);
-        }
-    
-    })    
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+})    
+
+
 
     btn.addEventListener(`click`, () =>{
-        size = prompt("Enter a size of grid")
-        createGrid(size)
+        size = prompt("Enter a grid dimensions that's in range 0 to 100")
+        while(size >= 100 || size < 0){
+        size = prompt("Enter a grid dimensions that's in range 0 to 100");
+    }
+        createGrid(size);
         let pen = document.querySelectorAll(".row");
-        console.log(pen.length)
-        /*let again = document.querySelectorAll(".row")[0]
-        while(content.again)
-        content.removeChild(again)*/
-        
+        console.log(pen)
         pen.forEach((i) => {
             i.addEventListener('mousedown', () =>{
-                i.style.backgroundColor="green"
-                clicked = true;
+                i.style.backgroundColor="black"
+                trailStart = true;
             });   
             i.addEventListener(`mouseup`, () => {
-                clicked = false;
+                trailStart = false;
             })
             i.addEventListener(`mouseover`, () =>{
-                if(clicked === true){
-                i.style.backgroundColor="green"
+                if(trailStart === true){
+                i.style.backgroundColor="black"
                 }
             });    
         });
-    });
+});
+
