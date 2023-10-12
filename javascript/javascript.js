e0bf82434d44fcdc21466b624a536bf0274fd743
AlesLearningCode <146716296+AlesLearningCode@users.noreply.github.com>
2023-10-12 20:03:22 +0200

    Make a loop, so old grid dimensions get deleted
    Made a loop so grid gets deleted and new one can get imported,
    have to maake it so, everything can be done through one click

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,36 +2,48 @@ const content = document.querySelector(`#container`);
 const btn = document.querySelector(`button`)
 let size;
 let clicked;
+let secondClick
 
     function createGrid(size){
-    for(let i = 0; i < (size * size); i++){
-    gridRow = document.createElement(`div`);
-    content.appendChild(gridRow);
-    gridRow.classList.add(`row`)  
-    gridRow.style.height = `${960 / size}px`;  
-    gridRow.style.width = `${960 / size}px`;   
-}
+        for(let i = 0; i < (size * size); i++){
+            gridRow = document.createElement(`div`);
+            content.appendChild(gridRow);
+            gridRow.classList.add(`row`)  
+            gridRow.style.height = `${960 / size}px`;  
+            gridRow.style.width = `${960 / size}px`;   
+    }
 }
     
     
 
-    btn.addEventListener(`click`, () =>{
+    btn.addEventListener(`dblclick`, () =>{
         size = prompt("Enter a size of grid")
         createGrid(size)
-        let pen = document.querySelectorAll(".row")
+        let pen = document.querySelectorAll(".row");
+        console.log(pen.length)
+        /*let again = document.querySelectorAll(".row")[0]
+        while(content.again)
+        content.removeChild(again)*/
+        
         pen.forEach((i) => {
-        i.addEventListener('mousedown', () =>{
-            i.style.backgroundColor="green"
-            clicked = true;
-        });   
-        i.addEventListener(`mouseup`, () => {
-            clicked = false;
-        })
-        i.addEventListener(`mouseover`, () =>{
-            if(clicked === true){
-            i.style.backgroundColor="green"
-            }
-        });    
+            i.addEventListener('mousedown', () =>{
+                i.style.backgroundColor="green"
+                clicked = true;
+            });   
+            i.addEventListener(`mouseup`, () => {
+                clicked = false;
+            })
+            i.addEventListener(`mouseover`, () =>{
+                if(clicked === true){
+                i.style.backgroundColor="green"
+                }
+            });    
+        });
     });
-    });
-    
+
+
+    btn.addEventListener(`click`, () => {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+    })    
